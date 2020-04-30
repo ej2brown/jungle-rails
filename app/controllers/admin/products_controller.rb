60e5371733a35473080492c09e9c8c2b9f0cc160
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-29 23:18:40 -0400

    Fix Bug: Missing Admin Security

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USERNAME"], password: ENV["HTTP_BASIC_PASSWORD"]
+ 
+  # name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  
   def index
     @products = Product.order(id: :desc).all
   end
