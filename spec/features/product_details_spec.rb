1eac68a807b860909addae1b7a47d99c2bd04900
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-05-05 16:09:33 -0400

    spec test to view product detail page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+    
+  end
+  scenario "They see a products deatails" do
+    # ACT
+    visit root_path
+    expect(page).to have_css 'article.product', count: 10
+    first('.product').click_link('Details') 
+    
+    # DEBUG
+    # save_screenshot
+    
+    # VERIFY
+
+    expect(page).to have_css 'section.products-show', count: 1
+
+    end
+end
